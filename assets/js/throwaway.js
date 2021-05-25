6ff96d8ca42971f262c99368a9ae04436f3bc5ae
Dylan Lalonde <contact@dylanlalonde.com>
2021-05-25 14:11:36 -0600

    changes to projectsMenu function to better fit project + some attempts to dynamically populate modals in throwaway.js

--- assets/js/throwaway.js
+++ assets/js/throwaway.js
@@ -1,26 +1,19 @@
+
 /*
 =============== 
-Global Variables
+Projects Modals Dynamic Content
 ===============
+
+when page loads add empty modal to page, hidden by default
+when 'open modal' button clicked, check id
+populate modal with content of corresponding id
+display modal
+when clicking exit, remove populated content & hide modal
+
 */
-// used in setDate()
-const date = document.getElementById("date");
 
-// used in closeLinks()
-const navToggle = document.querySelector(".nav-toggle");
-const linksContainer = document.querySelector(".links-container");
-const links = document.querySelector(".links");
 
-// used in fixedNavbar()
-const navbar = document.getElementById("nav");
-
-// used in smoothScroll()
-const scrollLinks = document.querySelectorAll(".scroll-link");
-// const navbar (defined above)
-// const linksContainer (defined above)
-
-// used in projectsMenu()
-const projects = [
+const modalItemsContent = [
   {
     id: 1,
     title: "buttermilk pancakes",
@@ -105,9 +98,9 @@ const projects = [
     price: 12.99,
     img: "item-8.jpeg",
     desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
-    history: `I'm baby wolf pickled schlitz try-hard normcore marfa man bun mumblecore vice pop-up XOXO lomo kombucha glossier bicycle rights. Umami kinfolk salvia jean shorts offal venmo. Knausgaard tilde try-hard, woke fixie banjo man bun. Small batch tumeric mustache tbh wayfarers 8-bit shaman chartreuse tacos. Viral direct trade hoodie ugh chambray, craft beer pork belly flannel tacos single-origin coffee art party migas plaid pop-up.`,
-    vision: `Man bun PBR&B keytar copper mug prism, hell of helvetica. Synth crucifix offal deep v hella biodiesel. Church-key listicle polaroid put a bird on it chillwave palo santo enamel pin, tattooed meggings franzen la croix cray. Retro yr aesthetic four loko tbh helvetica air plant, neutra palo santo tofu mumblecore. Hoodie bushwick pour-over jean shorts chartreuse shabby chic. Roof party hammock master cleanse pop-up truffaut, bicycle rights skateboard affogato readymade sustainable deep v live-edge schlitz narwhal.`,
-    goals: `Chambray authentic truffaut, kickstarter brunch taxidermy vape heirloom four dollar toast raclette shoreditch church-key. Poutine etsy tote bag, cred fingerstache leggings cornhole everyday carry blog gastropub. Brunch biodiesel sartorial mlkshk swag, mixtape hashtag marfa readymade direct trade man braid cold-pressed roof party. Small batch adaptogen coloring book heirloom. Letterpress food truck hammock literally hell of wolf beard adaptogen everyday carry. Dreamcatcher pitchfork yuccie, banh mi salvia venmo photo booth quinoa chicharrones.`,
+    history: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    vision: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    goals: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
   },
   {
     id: 9,
@@ -132,291 +125,86 @@ const projects = [
     goals: `Chambray authentic truffaut, kickstarter brunch taxidermy vape heirloom four dollar toast raclette shoreditch church-key. Poutine etsy tote bag, cred fingerstache leggings cornhole everyday carry blog gastropub. Brunch biodiesel sartorial mlkshk swag, mixtape hashtag marfa readymade direct trade man braid cold-pressed roof party. Small batch adaptogen coloring book heirloom. Letterpress food truck hammock literally hell of wolf beard adaptogen everyday carry. Dreamcatcher pitchfork yuccie, banh mi salvia venmo photo booth quinoa chicharrones.`,
   }
 ];
+const modalContent = document.querySelector(".modal-content");
 
-const sectionCenter = document.querySelector(".section-center");
-const container = document.querySelector(".btn-container");
-
-// used in projectModals()
-const modalOverlay = document.querySelector(".modal-overlay");
-
-
-/*
-=============== 
-Set Date
-===============
-*/
-var setDate = function () {
-  date.innerHTML = new Date().getFullYear();
-};
-
-setDate();
-
-
-/*
-=============== 
-Close Links
-===============
-
-// global variables accessed
-navToggle, linksContainer, links
-
-// notes
-Element.getBoundingClientRect() method returns the size of an element 
-and its position relative to the viewport.
-*/
-
-
-var closeLinks = function () {
-  navToggle.addEventListener("click", function() {
-    const containerHeight = linksContainer.getBoundingClientRect().height;
-    const linksHeight = links.getBoundingClientRect().height;
-    if (containerHeight === 0) {
-      linksContainer.style.height = `${linksHeight}px`;
-    } else {
-      linksContainer.style.height = 0;
-    }
-  });
-}
-
-closeLinks();
-
-
-/*
-=============== 
-Fixed Navbar
-===============
-
-// global variables accessed
-navbar
-
-
-// notes
-Element.getBoundingClientRect() method returns the size of an element 
-and its position relative to the viewport.
-
-Element.pageYOffset is a read - only window property that returns the 
-number of pixels the document has been scrolled vertically.
-
-*/
-
-
-var fixedNavbar = function () {
-  const topLink = document.querySelector(".top-link");
-  
-  window.addEventListener("scroll", function() {
-    const scrollHeight = window.pageYOffset;
-    const navHeight = navbar.getBoundingClientRect().height;
-    if (scrollHeight > navHeight) {
-      navbar.classList.add("fixed-nav");
-    } else {
-      navbar.classList.remove("fixed-nav");
-    }
-  
-    if (scrollHeight > 500) {
-      topLink.classList.add("show-link");
-    } else {
-      topLink.classList.remove("show-link");
-    }
-  });
-}
-
-fixedNavbar();
-
-
-/*
-=============== 
-Smooth Scroll
-===============
-
-// global variables accessed
-scrollLinks, navbar & linksContainer
-
-// notes
-String.slice extracts a section of a string without modifying original 
-string
-
-Element.offsetTop - A Number, representing the top position of the 
-element, in pixels
-
-*/
-var smoothScroll = function () {
-    scrollLinks.forEach(function (link) {
-    link.addEventListener("click", function (event) {
-      // prevent default
-      event.preventDefault();
-      // navigate to specific spot
-      const id = event.currentTarget.getAttribute("href").slice(1);
-      const element = document.getElementById(id);
-      // calculate the heights
-      const navHeight = navbar.getBoundingClientRect().height;
-      const containerHeight = linksContainer.getBoundingClientRect().height;
-      const fixedNav = navbar.classList.contains("fixed-nav");
-      let position = element.offsetTop - navHeight;
-
-      if (!fixedNav) {
-        position = position - navHeight;
-      }
-
-      if (navHeight > 82) {
-        position = position + containerHeight;
-      }
-      
-      window.scrollTo({
-        left: 0,
-        top:position
-      });
-      linksContainer.style.height = 0;
-    });
-  });
-}
-
-smoothScroll();
-
-
-/*
-=============== 
-Projects Menu
-===============
-
-// global variables accessed
-menu, sectionCenter, container
-
-*/
-
-var projectsMenu = function () {
-
-  // load items
-  window.addEventListener('DOMContentLoaded', () => {
-    displayProjectMenuItems(projects);
-    displayProjectFilterBtns();
-  });
-
-  function displayProjectMenuItems (projects) {
-    let displayProjectsMenu = projects.map( (project) => {
-      return `<article class="project-item">
-      <img src="./assets/img/projects/${project.img}" class="photo" alt="${project.title}">
-      <div class="project-info">
-        <header>
-          <h4>${project.title}</h4>
-          <button class="btn modal-btn" data-id="${project.id}">
-          open modal
-        </button>
-        </header>
-        <p class="project-text">
-          ${project.desc}
-        </p>
-      </div>
-    </article>`;
-    });
-    displayProjectsMenu = displayProjectsMenu.join("");
-    sectionCenter.innerHTML = displayProjectsMenu;
-  }
-
-  function displayProjectFilterBtns() {
-    const categories = projects.reduce( (values, item) => {
-      if (!values.includes(item.category)) {
-        values.push(item.category);
-      }
-      return values;
-    }, ['all'] );
-
-    const categoryBtns = categories.map( (category) => {
-      return `<button class="filter-btn" type="button" data-id="${category}">${category}</button>`
-    }).join("");
-    container.innerHTML = categoryBtns;
-    const filterBtns = container.querySelectorAll(".filter-btn");
-    
-    // filter items
-    filterBtns.forEach( (btn) => {
-      btn.addEventListener("click", (event) => {
-        const category = event.currentTarget.dataset.id;
-        const projectCategory = projects.filter( (project) => {
-          if (project.category === category) {
-            return project;
-          }
-        });
-        if (category === "all") {
-          displayProjectMenuItems(projects);
-        } else {
-          displayProjectMenuItems(projectCategory);
-        }
-      });
-    });
-  }
-
-}
-
-projectsMenu();
-
-
-/*
-=============== 
-Global Event Listener
-===============
-*/
-
-function addGlobalEventListener(type, selector, callback) {
-  document.addEventListener(type, e => {
-    if (e.target.matches(selector)) callback(e)
-  });
-}
-
-
-/*
-=============== 
-Projects Modals
-===============
-*/
-
-var projectsModals = function () {
-
+function loadModalContent (modalItemsContent, modalContent) {
   addGlobalEventListener("click", ".btn.modal-btn", e => {
-    if (e.target.matches(".btn.modal-btn")) {
-      modalOverlay.classList.add("open-modal");
-    }
-  });
+    let itemId = e.target.attributes[1].value;
+    let displayModalContent = modalItemsContent.map(function(item) {
 
-  addGlobalEventListener("click", ".fas.fa-times", e => {
-    if (e.target.parentElement.matches("button.close-btn")) {
-      modalOverlay.classList.remove("open-modal");
-    }
-  });
+      console.log("button id: " + itemId)
+      console.log("modalitemcontent id: " + item.id)
 
+      if (itemId === item.id) {
+        return `<article class="modal-content">
+        <!-- single item -->
+        <div class="modal-content-item active" id="history">
+          <h4>history</h4>
+          <p>${item.history}</p>
+        </div>
+        <!-- end of single item -->
+        <!-- single item -->
+        <div class="modal-content-item" id="vision">
+          <h4>vision</h4>
+          <p>${item.vision}</p>
+        </div>
+        <!-- end of single item -->
+        <!-- single item -->
+        <div class="modal-content-item" id="goals">
+          <h4>goals</h4>
+          <p>${item.goals}</p>
+        </div>
+        <!-- end of single item -->
+        </article>`
+      }
+      // console.log(displayModalContent)
+      displayModalContent = displayModalContent.join("");
+      console.log(displayModalContent)
+      console.log(modalContent)
+      modalContent.innerHTML = displayModalContent;
+      console.log(modalContent)
+    });
+
+  });
 }
 
-projectsModals();
+loadModalContent(modalItemsContent, modalContent);
+
+// addGlobalEventListener("click", ".btn.modal-btn", e => {
+  
+//   // if (e.target.contains(`data-id="3"`)) {
+//   //   console.log(true);
+//   // }
+  
+//   let itemId = e.target.attributes[1].value;
+
+//   console.log(itemId);
+
+//   if (e.target.matches(".btn.modal-btn")) {
+//     modalOverlay.classList.add("open-modal");
+//   }
+// });
 
 
-/*
-=============== 
-Projects Modals Tabs
-===============
+// const modalBtns = document.querySelectorAll(".btn.modal-btn");
+// console.log(modalBtns);
 
-// global variables accessed
-modalTabBtns, modalBody, modalContentItems
+// modalBtns.forEach(function (btn) {
+//   btn.addEventListener("click", function (event) {
 
-*/
+//     console.log("working")
+    // console.log(event.currentTarget.dataset.id)
 
-const modalTabBtns = document.querySelectorAll(".tab-btn");
-const modalBody = document.querySelector(".modal-body");
-const modalContentItems = document.querySelectorAll(".modal-content-item");
-
-modalBody.addEventListener("click", function (event) {
-  const id = event.target.dataset.id;
-  if (id) {
-    // remove active from other buttons
-    modalTabBtns.forEach(function(btn) {
-      btn.classList.remove("active");
-      // add active class to target button
-      event.target.classList.add("active");
-    });
-    // hide all modalContentItems
-    modalContentItems.forEach(function(article){
-      article.classList.remove("active");
-    });
-    // show active article
-    const element = document.getElementById(id);
-    element.classList.add("active");
-  }
-})
-
+    // const category = event.currentTarget.dataset.id;
+    // const menuCategory = menu.filter(function (menuItem) {
+    //   if (menuItem.category === category) {
+    //     return menuItem;
+    //   }
+    // });
+    // if (category === "all") {
+    //   displayMenuItems(menu);
+    // } else {
+    //   displayMenuItems(menuCategory);
+    // }
+//   });
+// });
